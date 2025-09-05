4d46789ba6386f6acecf739df33295367c2f11ab
Ban68 <carlosvives89@hotmail.com>
2025-09-05 17:02:47 +0200

    DevLocal

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"DI6ACaSK6VQcTN0/k9tvv8I9yccJ8D7Uw3dUBcKb2+I=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{\"a5268647550920947e20ff1df10ae82bbcd774ec\":{\"workers\":{\"app/onboarding/page\":\"8023\"},\"layer\":{\"app/onboarding/page\":\"rsc\"}}},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
